9ccf7314de1106f799eec4c1cb0d65cf03fb33d6
Mateusz Rojek <Merkius@wp.pl>
2023-02-14 00:50:30 +0100

    components and inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import "./App.css";
+import { Time } from "./Time";
+import React, { useState } from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [workTime, setWorkTime] = useState("");
+  const [restTime, setRestTime] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(`Work time: ${workTime}`);
+    console.log(`Rest time: ${restTime}`);
+  };
+
+  const handleWorkTimeChange = (event) => {
+    setWorkTime(event.target.value);
+  };
+
+  const handleRestTimeChange = (event) => {
+    setRestTime(event.target.value);
+  };
+  return (
+    <div>
+      <div class="flex items-stretch">
+        <form onSubmit={handleSubmit}>
+          <label for="workt">Work time:</label>
+          <input
+            type="number"
+            id="workt"
+            name="workt"
+            onChange={handleWorkTimeChange}
+          />
+          <label for="restt">Rest time:</label>
+          <input
+            type="number"
+            id="restt"
+            name="restt"
+            onChange={handleRestTimeChange}
+          />
+          <button type="submit">Confirm</button>
+        </form>
+      </div>
+      <Time></Time>
+    </div>
+  );
 }
 
 export default App;
