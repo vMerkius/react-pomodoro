2c201724e6fe6dcc77c2378c39b45f566ab0a7bb
Mateusz Rojek <Merkius@wp.pl>
2023-02-17 18:32:19 +0100

    working app- buttons,alarm,timer

--- src/App.js
+++ src/App.js
@@ -2,38 +2,83 @@ import { Time } from "./Time";
 import React, { useState } from "react";
 
 function App() {
-  const [workTime, setWorkTime] = useState(25);
-  const [restTime, setRestTime] = useState(5);
-  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [time, setTime] = useState(0);
+  const [bgColor, setBgColor] = useState("gray-100");
 
-  const handleConfirm = (event) => {
-    event.preventDefault();
-    setIsConfirmed(true);
+  const handleButtonWork = () => {
+    setTime(25);
+    document.body.classList.remove("bg-[#acc9cf]", "bg-[#7fbf7f]");
+    document.body.classList.add(
+      "bg-[#eca3a3]",
+      "transition-all",
+      "duration-500"
+    );
+    setBgColor("red");
+  };
+  const handleButtonShortRest = () => {
+    setTime(5);
+    document.body.classList.remove("bg-[#eca3a3]", "bg-[#7fbf7f]");
+    document.body.classList.add(
+      "bg-[#acc9cf]",
+      "transition-all",
+      "duration-500"
+    );
+    setBgColor("blue");
+  };
+
+  const handleButtonLongRest = () => {
+    setTime(15);
+    document.body.classList.remove("bg-[#acc9cf]", "bg-[#eca3a3]");
+    document.body.classList.add(
+      "bg-[#7fbf7f]",
+      "transition-all",
+      "duration-500"
+    );
+    setBgColor("green");
   };
   return (
     <div>
-      <div class="flex items-stretch">
-        <form onSubmit={handleConfirm}>
-          <label htmlFor="workt">Work time:</label>
-          <input
-            type="number"
-            id="workt"
-            name="workt"
-            value={workTime}
-            onChange={(event) => setWorkTime(event.target.value)}
-          />
-          <label htmlFor="restt">Rest time:</label>
-          <input
-            type="number"
-            id="restt"
-            name="restt"
-            value={restTime}
-            onChange={(event) => setRestTime(event.target.value)}
-          />
-          <button type="submit">Confirm</button>
-        </form>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div
+          className={`rounded-lg py-24 px-4 ${
+            bgColor === "gray-100"
+              ? "bg-[#ddd]"
+              : bgColor === "red"
+              ? "bg-[#e68484]"
+              : bgColor === "blue"
+              ? "bg-[#91b7bf]"
+              : "bg-[#4ca64c]"
+          }`}
+        >
+          {/* //#e68484 #91b7bf #4ca64c */}
+          <div className="flex">
+            <button
+              type="button"
+              className="px-8 py-2 bg-[#cccccc] text-[#222] rounded-3xl text-xl mr-4 hover:bg-[#666666] hover:text-[#ddd] "
+              onClick={handleButtonWork}
+            >
+              Work
+            </button>
+            <button
+              type="button"
+              className="px-8 py-2 bg-[#cccccc] text-[#222] rounded-3xl text-xl mr-4 hover:bg-[#666666] hover:text-[#ddd]"
+              onClick={handleButtonShortRest}
+            >
+              Short Rest
+            </button>
+            <button
+              type="button"
+              className="px-8 py-2 bg-[#cccccc] text-[#222] rounded-3xl text-xl hover:bg-[#666666] hover:text-[#ddd]"
+              onClick={handleButtonLongRest}
+            >
+              Long Rest
+            </button>
+          </div>
+          <div className="mt-32 flex flex-col items-center">
+            {<Time work={time} />}
+          </div>
+        </div>
       </div>
-      {isConfirmed && <Time work={workTime} rest={restTime} />}
     </div>
   );
 }
--- src/Time.js
+++ src/Time.js
@@ -1,44 +1,44 @@
 import React, { useState, useEffect, useRef } from "react";
 
-function Time({ work = 25, rest = 5 }) {
+function Time({ work }) {
   const [time, setTime] = useState(work * 60);
-  const [isWorking, setIsWorking] = useState(true); // flaga, czy aktualnie trwa czas pracy
-  const prevIsWorkingRef = useRef(isWorking);
+  const audioRef = useRef(null);
 
   useEffect(() => {
-    prevIsWorkingRef.current = isWorking;
-  }, [isWorking]);
-
-  useEffect(() => {
-    setTime(isWorking ? work * 60 : rest * 60);
-  }, [isWorking, work, rest]);
+    setTime(work * 60);
+  }, [work]);
 
   useEffect(() => {
     const countdown = setInterval(() => {
       setTime((prevTime) => {
         if (prevTime === 0) {
-          setIsWorking((prevIsWorking) => {
-            prevIsWorkingRef.current = prevIsWorking;
-            return !prevIsWorking;
-          });
-          return prevIsWorkingRef.current ? rest * 60 : work * 60;
+          audioRef.current.play(); // uruchomienie dźwięku alarmu
+          return work * 0;
         } else {
           return prevTime - 1;
         }
       });
     }, 1000);
     return () => clearInterval(countdown);
-  }, [work, rest]);
+  }, [work]);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        {isWorking ? "Work" : "Rest"} time left: {minutes}:
-        {seconds < 10 ? "0" : ""}
-        {seconds}
+      <h1 className="text-3xl">
+        <div className="flex items-center">
+          <div className="">Time:</div>
+          <div className="ml-2">
+            {minutes}:{seconds < 10 ? "0" : ""}
+            {seconds}
+          </div>
+        </div>
       </h1>
+      <audio ref={audioRef}>
+        <source src="/alarm.mp3" type="audio/mpeg" />
+      </audio>
     </div>
   );
 }
